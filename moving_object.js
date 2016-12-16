664be7ae811a91ecdaff7ec8db6f20ea369f0558
Hui Jun Yam <hjyam@umich.edu>
2016-12-16 12:11:51 -0800

    Complete MovingObject class

---
+++ moving_object.js
@@ -0,0 +1,34 @@
+function MovingObject (option) {
+  this.pos = option["pos"];
+  this.vel = option["vel"];
+  this.radius = option["radius"];
+  this.color = option["color"];
+}
+
+MovingObject.prototype.draw = function (ctx) {
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+
+  ctx.arc(
+    this.pos[0],
+    this.pos[1],
+    this.radius,
+    0,
+    2 * Math.PI,
+    false
+  );
+
+  ctx.fill();
+};
+
+MovingObject.prototype.move = function () {
+  this.pos = [this.pos[0] + this.vel[0], this.pos[1] + this.vel[1]];
+};
+
+module.exports = MovingObject;
+
+// const mo = new MovingObject(
+//   { pos: [30, 30], vel: [10, 10], radius: 5, color: "#00FF00"}
+// );
+//
+// console.log(mo);
